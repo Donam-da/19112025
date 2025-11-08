2e401fb71cd2afa2a2e2d6f09367d4e0c008686b
Donam-da <22014002@st.phenikaa-uni.edu.vn>
2025-11-08 08:46:39 +0700

    ảnh icon đăng nhập đã giống với ảnh chìm của hóa đơn thanh toán

--- namm/InvoiceWindow.xaml.cs
+++ namm/InvoiceWindow.xaml.cs
@@ -1,15 +1,26 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace namm
 {
     public partial class InvoiceWindow : Window
     {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["CafeDB"].ConnectionString;
+
         public InvoiceWindow(int tableId, string tableName, string customerName, string customerCode, decimal subTotal, decimal discountPercent, decimal finalTotal, ObservableCollection<BillItem> billItems, int billId)
         {
             InitializeComponent();
+
+            // Tải và thiết lập ảnh nền động
+            _ = SetBackgroundImageAsync();
 
             // Điền thông tin vào hóa đơn
             tbInvoiceId.Text = billId.ToString("D6"); // Định dạng số hóa đơn, ví dụ: 000123
@@ -30,6 +41,54 @@ namespace namm
 
             // Hiển thị danh sách món
             dgBillItems.ItemsSource = billItems;
+        }
+
+        private async Task SetBackgroundImageAsync()
+        {
+            try
+            {
+                byte[]? imageData = null;
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    // Lấy ảnh đang được kích hoạt cho màn hình đăng nhập
+                    var command = new SqlCommand("SELECT ImageData FROM InterfaceImages WHERE IsActiveForLogin = 1", connection);
+                    imageData = await command.ExecuteScalarAsync() as byte[];
+                }
+
+                if (imageData != null && imageData.Length > 0)
+                {
+                    var imageSource = await Task.Run(() => LoadImageFromBytes(imageData));
+                    var imageBrush = new ImageBrush(imageSource)
+                    {
+                        Stretch = Stretch.UniformToFill,
+                        Opacity = 0.15 // Giữ độ mờ để làm ảnh chìm
+                    };
+                    imageBrush.Freeze(); // Tối ưu hóa hiệu suất
+                    InvoiceGrid.Background = imageBrush;
+                }
+                // Nếu không tìm thấy ảnh, Grid sẽ có nền trong suốt mặc định
+            }
+            catch (Exception)
+            {
+                // Bỏ qua lỗi, không hiển thị ảnh nền nếu có sự cố
+            }
+        }
+
+        private BitmapImage LoadImageFromBytes(byte[] imageData)
+        {
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
         }
 
         private void BtnConfirm_Click(object sender, RoutedEventArgs e)
